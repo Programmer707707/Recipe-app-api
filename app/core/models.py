fdf1bfc3c6d6cb8c9ce275bbfff9e8bf051c6809
Programmer707707 <toxirovsherzod707@gmail.com>
2025-07-18 13:21:10 +0500

    Recipe Model and API has been created & completed

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django.conf import settings
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -31,3 +32,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return self.email
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    
+    def __str__(self):
+        return self.title
