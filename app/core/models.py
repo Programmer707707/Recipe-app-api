f92a04e42127f707334e5b5153ba78c1964e8de9
Programmer707707 <toxirovsherzod707@gmail.com>
2025-07-19 21:33:43 +0500

    Image uploading feature has been added

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,16 @@
+import os
+import uuid
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+    
+    return os.path.join('uploads', 'recipe', filename)
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -42,13 +52,24 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
     
     def __str__(self):
         return self.title
     
 class Tag(models.Model):
+    """Tag for filtering recipes"""
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     
+    def __str__(self):
+        return self.name
+    
+class Ingredient(models.Model):
+    """Ingredient for recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,10 +2,12 @@
 Views for Recipe APIs.
 """
 
-from rest_framework import (viewsets, mixins)
+from rest_framework import (viewsets, mixins, status)
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -21,18 +23,41 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
+        
         return self.serializer_class
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
         
-class TagViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
-    """Manage tags in database."""
-    serializer_class = serializers.TagSerializer
-    queryset = Tag.objects.all()
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+class BaseRecipeAttrViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    """Base class for Tag and Ingredient viewsets"""
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
+        
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+   
     
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database"""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
